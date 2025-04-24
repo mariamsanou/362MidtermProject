768bcdcbbfe542ee97ce02aa2fb6ef5e9574e006
Mariam Sanou <79387601+mariamsanou@users.noreply.github.com>
2025-04-24 18:21:51 -0400

    Delete client directory

--- client/js/showReviewForm.js
+++
@@ -1,9 +0,0 @@
-// showReviewForm.js
-
-function showReviewForm(restaurantId) {
-  document.getElementById(`review-form-${restaurantId}`).style.display = 'block';
-}
-
-function hideReviewForm(restaurantId) {
-  document.getElementById(`review-form-${restaurantId}`).style.display = 'none';
-}
--- client/js/showReviews.js
+++
@@ -1,6 +0,0 @@
-function showReviews(restaurantId) {
-    const reviewList = document.getElementById(`reviews-${restaurantId}`);
-    reviewList.style.display = reviewList.style.display === "none" ? "block" : "none";
-
-    // Optional: Refresh reviews if needed (you can add real API fetch later)
-}
