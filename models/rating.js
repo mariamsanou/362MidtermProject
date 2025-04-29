afacb870a9a01914406165f1387b28eb0a55d44f
Mariam Sanou <79387601+mariamsanou@users.noreply.github.com>
2025-04-29 18:40:55 -0400

    Update rating.js

--- models/rating.js
+++ models/rating.js
@@ -1,11 +1,14 @@
+
 const mongoose = require('mongoose');
 
-const ratingSchema = new mongoose.Schema({
-  locationId: { type: String, required: true },
-  name: { type: String, required: true },  // first name only
-  comment: { type: String, required: true },
-  rating: { type: Number, min: 1, max: 5, required: true },
-  createdAt: { type: Date, default: Date.now }
+const RatingSchema = new mongoose.Schema({
+  place: String,
+  rating: Number,
+  comment: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('Rating', ratingSchema);
+module.exports = mongoose.model('Rating', RatingSchema);
